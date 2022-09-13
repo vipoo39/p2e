5779ce7255f6e31cf05a486afb6942b1cce9bc21
podpison <au25557@yandex.ru>
2022-09-13 19:00:38 +0500

    update 7

--- src/pages/OrderPage/OrderPage.tsx
+++ src/pages/OrderPage/OrderPage.tsx
@@ -14,7 +14,7 @@ import { GAMES_URL } from './../../utils/links';
 export default function OrderPage() {
     const location = useLocation<{ back: string; game: string; avatar: string; nikname: string, online: string, category: BreadcrumbsItemType | null }>()
     const history = useHistory()
-    let breadcrumbItems = [{ name: location.state?.game, link: `${GAMES_URL}/${location.state?.game}` } || null, location.state?.category ? location.state?.category : null,  { name: location.state?.back, link: null }].filter(i => i !== null)
+    let breadcrumbItems = [{ name: location.state?.game, link: `${GAMES_URL}/${location.state?.game}` } || null, location.state?.category ? location.state?.category : null].filter(i => i !== null)
     //@ts-ignore
     useBreadcrumbs(breadcrumbItems)
     const [name, setName] = useState('')
