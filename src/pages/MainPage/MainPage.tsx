c41988e9a3391f483e495a1802798c123eab6799
podpison <au25557@yandex.ru>
2022-09-13 19:59:17 +0500

    update 7 fix 2

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -72,6 +72,11 @@ export default function MainPage() {
                     <SearchResultList letter='A' />
                     <SearchResultList letter='B' />
                     <SearchResultList letter='C' />
+                    <SearchResultList letter='D' />
+                    <SearchResultList letter='E' />
+                    <SearchResultList letter='F' />
+                    <SearchResultList letter='G' />
+                    <SearchResultList letter='H' />
                 </div>
             </div>
             <FAQ />
