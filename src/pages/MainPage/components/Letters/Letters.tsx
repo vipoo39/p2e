4ca8f1d6d07c1b5a811b9eb3a67e5bc0c6469118
podpison <au25557@yandex.ru>
2022-09-15 13:45:01 +0500

    5

--- src/pages/MainPage/components/Letters/Letters.tsx
+++ src/pages/MainPage/components/Letters/Letters.tsx
@@ -11,12 +11,13 @@ export default function Letters({ handleClick }: LettersProps) {
     const containerRef = useRef<HTMLDivElement>(null)
 
     useEffect(() => {
-        if (containerRef?.current && window.innerHeight - 50 <= containerRef.current.clientHeight) {
+        if (containerRef?.current && window.innerHeight <= containerRef.current.clientHeight) {
             const handler = () => {
                 var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
                 if (st > prevPos){
                     if (containerRef.current) {
-                        containerRef.current.style.top = '-100px'
+                        let topPos = window.innerHeight - containerRef.current.clientHeight + 'px'
+                        containerRef.current.style.top = topPos
                     }
                 } else {
                     if (containerRef.current) {
